d8b2a4e43d8fc34737236774701f22e02069c3ca
Dmytrii Holiaka <holyaka_d_m@ukr.net>
2023-01-14 23:10:01 +0200

    Start task #1 HW2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,47 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+// import { styled } from 'styled-component';
+
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  onClick = evt => {
+    const { name } = evt.target;
+    let value = this.state[name];
+    value += 1;    
+    this.setState(preState => preState[name] = value);
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          height: '100vh',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: 40,
+          color: '#010101',
+        }}
+      >
+        <h2>Please leave feedback</h2>
+        <div onClick={this.onClick}>
+          <button name='good'>Good</button>
+          <button name='neutral'>Neutral</button>
+          <button name='bad'>Bad</button>
+        </div>
+        <h2>Statistics</h2>
+        <div>
+          <p>Good: {this.state.good}</p>
+          <p>Neutral: {this.state.neutral}</p>
+          <p>Bad: {this.state.bad}</p>
+        </div>
+      </div>
+    );
+  }
+}
